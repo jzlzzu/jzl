567066b2470711f929ba092901e694573c818724
zll <291928588@qq.com>
2019-03-25 16:08:58 +0800

    策略模式

--- src/main/java/com/jzl/design/DesignTest.java
+++ src/main/java/com/jzl/design/DesignTest.java
@@ -2,6 +2,7 @@ package com.jzl.design;
 
 import com.jzl.design.factory.Calculator;
 import com.jzl.design.factory.CalculatorFactory;
+import com.jzl.design.strategy.CashContext;
 import org.junit.Test;
 
 /**
@@ -23,5 +24,12 @@ public class DesignTest {
         calculator.b = 5;
         double result = calculator.getResult();
         System.out.println(result);
+    }
+
+    @Test
+    public void testStrategy() {
+        CashContext cashContext = new CashContext("dazhe");
+        double result = cashContext.getResult(1000);
+        System.out.println("打折后价钱_" + result);
     }
 }
