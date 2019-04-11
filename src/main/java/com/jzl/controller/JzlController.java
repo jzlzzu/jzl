ac7d63986c2f22227bf2c7c2b6502d5bbedd511d
jzl <958136981@qq.com>
2019-04-11 18:19:08 +0800

    测试HttpServletResponse

--- src/main/java/com/jzl/controller/JzlController.java
+++ src/main/java/com/jzl/controller/JzlController.java
@@ -1,7 +1,12 @@
 package com.jzl.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * @Author: jzl
@@ -12,9 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/jzl")
 public class JzlController {
 
+    @Autowired
+    private HttpServletResponse response;
+
     @GetMapping("get")
-    public void jzlGet(){
+    public void jzlGet() throws IOException {
+        OutputStream os = response.getOutputStream();
+        os.write("E1111cba".getBytes());
         System.out.println("调用get方法");
+//        return "getCC";
     }
 
 }
