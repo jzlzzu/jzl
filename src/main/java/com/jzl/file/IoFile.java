7732426541aedced3c0061a892614b1c99e1ac06
jzl <958136981@qq.com>
2019-07-12 16:45:17 +0800

    添加字节流测试类

--- src/main/java/com/jzl/file/IoFile.java
+++ src/main/java/com/jzl/file/IoFile.java
@@ -1,7 +1,7 @@
 package com.jzl.file;
 
 import java.io.*;
-import java.nio.Buffer;
+import java.util.Arrays;
 
 /**
  * @Author: jzl
@@ -25,14 +25,74 @@ public class IoFile {
 //        bufferedWriter();
 //        outputStreamWriter();
 
-        return;
 
 
         //字节流  inputStream fileInputStream bufferedInputStream
         //       outPutStream fileOutPutStream bufferedOutStream
+//        fileInputStream();
+//        bufferedInputStream();
+//        fileOutputStream();
+//        bufferedOutputStream();
+
+//        picCopyTest();
 
 
 
+    }
+
+    private static void picCopyTest() throws IOException {
+        //完成图片复制
+
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(new File("c:\\jzl\\pic.png")));
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(new File("c:\\jzl\\copy.png")));
+        byte[] by = new byte[64];
+        int len = 0;
+        while (-1 != (len = bufferedInputStream.read(by))){
+            bufferedOutputStream.write(by,0,len);
+        }
+        bufferedOutputStream.flush();
+        bufferedOutputStream.close();
+        bufferedInputStream.close();
+    }
+
+    private static void bufferedOutputStream() throws IOException {
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(new File("c:\\jzl\\out.txt")));
+        bufferedOutputStream.write("olleh\r\n".getBytes());
+        bufferedOutputStream.write("你好".getBytes());
+        bufferedOutputStream.flush();
+        bufferedOutputStream.close();
+    }
+
+    private static void fileOutputStream() throws IOException {
+        // 写文件
+        FileOutputStream fileOutputStream = new FileOutputStream(new File("c:\\jzl\\out.txt"));
+        fileOutputStream.write("olleh\r\n".getBytes());
+        fileOutputStream.write("你好".getBytes());
+        fileOutputStream.flush();
+        fileOutputStream.close();
+    }
+
+    private static void bufferedInputStream() throws IOException {
+        //减少磁盘io操作
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(new File("c:\\jzl\\io.txt")));
+        byte[] cbuf = new byte[1024];
+        int len;
+        while(-1 != (len = bufferedInputStream.read(cbuf))){
+            System.out.println(new String(cbuf,0,len));
+        }
+        bufferedInputStream.close();
+    }
+
+    private static void fileInputStream() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(new File("c:\\jzl\\io.txt"));
+
+        byte[] cbuf = new byte[1024];
+        int len;
+        while(-1 != (len = fileInputStream.read(cbuf))){
+            System.out.println(new String(cbuf,0,len));
+        }
+        ;
+        fileInputStream.close();
     }
 
     private static void outputStreamWriter() throws IOException {
